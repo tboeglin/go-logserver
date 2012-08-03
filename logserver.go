25c6514ea7b752e172bb40d75f9a37281d0f6879
Thomas Boeglin <thomas.boeglin@gmail.com>
2012-08-03 13:18:35 +0200

    new flags for using a rotating file handler for logging

--- logserver.go
+++ logserver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"logserver/handlers"
+    "loghandler"
 	"net/http"
 )
 
@@ -12,12 +13,19 @@ func main() {
 	var (
 		portno  int
 		backlogsize int
+        logfile string
+        logsize int
 	)
 	flag.IntVar(&portno, "port", 8888, "the port to bind to")
 	flag.IntVar(&backlogsize, "backlogsize", 1000, "the size of the backlog to keep in memory for fetching over HTTP")
+    flag.StringVar(&logfile, "logfile", "STDERR", "the file to log to, by default the console on stderr")
+	flag.IntVar(&logsize, "logsize", 10485760, "the maximum size of the log file before it's rotated")
 
 	flag.Parse()
 	handlers.MaxLogSize(backlogsize)
+    if logfile != "STDERR" {
+        log.SetOutput(rotatingfile.Create(logfile, logsize, 10))
+    }
 	// handlers
 	http.HandleFunc("/log", handlers.HandleLogPost)
 	http.HandleFunc("/stats", handlers.HandleStats)
