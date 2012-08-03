189335d97ae7573dac90466aea66ea03fa871ac3
Thomas Boeglin <thomas.boeglin@gmail.com>
2012-08-03 13:18:08 +0200

    rename logsize command line option to backlogsize

--- logserver.go
+++ logserver.go
@@ -11,18 +11,18 @@ import (
 func main() {
 	var (
 		portno  int
-		logsize int
+		backlogsize int
 	)
 	flag.IntVar(&portno, "port", 8888, "the port to bind to")
-	flag.IntVar(&logsize, "logsize", 1000, "the size of the backlog")
+	flag.IntVar(&backlogsize, "backlogsize", 1000, "the size of the backlog to keep in memory for fetching over HTTP")
 
 	flag.Parse()
-	handlers.MaxLogSize(logsize)
+	handlers.MaxLogSize(backlogsize)
 	// handlers
 	http.HandleFunc("/log", handlers.HandleLogPost)
 	http.HandleFunc("/stats", handlers.HandleStats)
 
-    log.Printf("Starting on port %d with backlog size %d\n", portno, logsize)
+    log.Printf("Starting on port %d with backlog size %d\n", portno, backlogsize)
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", portno), nil)
 	if err != nil {
