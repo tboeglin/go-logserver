7e4a1942d981b87fb19c0b00e5b705a965128bb2
Thomas Boeglin <thomas.boeglin@gmail.com>
2012-08-03 10:49:37 +0200

    go back to a LogInfo structure Timestamp field is added locally by the server

--- handlers/loghandlers.go
+++ handlers/loghandlers.go
@@ -7,11 +7,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
+
+type LogInfo struct {
+	Timestamp time.Time // will be marshalled according to RFC3339
+	Severity  string
+	Source    string
+	Message   string
+}
 
 // we'll use a global channel for the logging goroutine to read from
 var (
-	log_chan chan string
+	log_chan chan LogInfo
 	// send channels to get data from through this one
 	request_chan chan (chan<- []byte)
 	requests     uint64
@@ -21,7 +29,7 @@ var (
 
 func init() {
 	// init the global log_chan and its reader
-	log_chan = make(chan string)
+	log_chan = make(chan LogInfo)
 	request_chan = make(chan (chan<- []byte))
 	maxRingSize = 1000
 	logs = ring.New(maxRingSize)
@@ -29,14 +37,14 @@ func init() {
 }
 
 func MaxLogSize(size int) {
-    // set the backlog size (and resets it in the process so call it at the very beginning)
-    logs = ring.New(size)
-    maxRingSize = size
+	// set the backlog size (and resets it in the process so call it at the very beginning)
+	logs = ring.New(size)
+	maxRingSize = size
 }
 
 func loggerRoutine() {
 	var (
-		li       string
+		li       LogInfo
 		out_chan chan<- []byte
 		enc      []byte
 		err      error
@@ -45,27 +53,26 @@ func loggerRoutine() {
 	for {
 		select {
 		case li = <-log_chan:
-			log.Printf("LOG \"%s\"", li)
-            logs.Value = li
+			log.Printf("[%s] from %s: %s\n", li.Severity, li.Source, li.Message)
+			logs.Value = li
 			logs = logs.Next()
 			//inc the global counter
 			requests += 1
 		case out_chan = <-request_chan:
-			log.Printf("Logs requested (%d in the ring)\n", logs.Len())
 			// get the element in a regular list
 			log_len = logs.Len()
-			lst := make([]string, log_len)
-            var index int = 0
-            logs.Do(func(elem interface{}) {
-                if elem != nil {
-                    //log.Printf("current index is %d\n", index)
-                    lst[index] = elem.(string)
-                    index += 1
-                }
-            })
+			lst := make([]LogInfo, log_len)
+			var index int = 0
+			logs.Do(func(elem interface{}) {
+				if elem != nil {
+					//log.Printf("current index is %d\n", index)
+					lst[index] = elem.(LogInfo)
+					index += 1
+				}
+			})
 			// reset the ring
 			logs = ring.New(maxRingSize)
-            enc, err = json.Marshal(lst[0:index])
+			enc, err = json.Marshal(lst[0:index])
 			if err != nil {
 				log.Printf("could not convert the logs to JSON !!!")
 			}
@@ -84,11 +91,11 @@ func HandleStats(rw http.ResponseWriter, r *http.Request) {
 
 func HandleLogPost(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	//try decoding the payload as json
 	case "POST":
 		var (
 			payload []byte
 			err     error
+			li      LogInfo
 		)
 		payload, err = ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -96,10 +103,21 @@ func HandleLogPost(rw http.ResponseWriter, r *http.Request) {
 			rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		//try decoding the payload as json
+		err = json.Unmarshal(payload, &li)
+		if err != nil {
+			log.Printf("could not unmarshal JSON \"%s\" from %s\n",
+				payload, r.RemoteAddr)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// add the timestamp
+		li.Timestamp = time.Now().UTC()
 		// send it to the logger
-		log_chan <- string(payload)
+		log_chan <- li
 		rw.WriteHeader(http.StatusOK)
 	case "GET":
+		log.Printf("logs requested from %s\n", r.RemoteAddr)
 		var logs_json []byte
 		logs_chan := make(chan []byte)
 		request_chan <- logs_chan
