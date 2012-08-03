b2abd75d3dc7101ea2cef3f02d165d839f08b2d0
Thomas Boeglin <thomas.boeglin@gmail.com>
2012-08-03 07:45:20 +0200

    make the backlog a ring buffer of strings

--- handlers/loghandlers.go
+++ handlers/loghandlers.go
@@ -1,46 +1,84 @@
 package handlers
 
 import (
+	"container/ring"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"container/ring"
 )
 
 // we'll use a global channel for the logging goroutine to read from
 var (
 	log_chan chan string
-    // send channels to get data from through this one
-    request_chan <-chan (chan<- []string)
-	requests uint64
-    logs *ring.Ring
+	// send channels to get data from through this one
+	request_chan chan (chan<- []byte)
+	requests     uint64
+	logs         *ring.Ring
+	maxRingSize  int
 )
 
 func init() {
 	// init the global log_chan and its reader
 	log_chan = make(chan string)
-    request_chan = make(<-chan (chan<- []string))
-    logs = ring.New(1000)
+	request_chan = make(chan (chan<- []byte))
+	maxRingSize = 1000
+	logs = ring.New(maxRingSize)
 	go loggerRoutine()
 }
 
+func MaxLogSize(size int) {
+    // set the backlog size (and resets it in the process so call it at the very beginning)
+    logs = ring.New(size)
+    maxRingSize = size
+}
+
 func loggerRoutine() {
-	var li string
+	var (
+		li       string
+		out_chan chan<- []byte
+		enc      []byte
+		err      error
+		log_len  int
+	)
 	for {
-		li = <-log_chan
-        log.Printf("LOG \"%s\"", li)
-		//inc the global counter
-		requests += 1
+		select {
+		case li = <-log_chan:
+			log.Printf("LOG \"%s\"", li)
+            logs.Value = li
+			logs = logs.Next()
+			//inc the global counter
+			requests += 1
+		case out_chan = <-request_chan:
+			log.Printf("Logs requested (%d in the ring)\n", logs.Len())
+			// get the element in a regular list
+			log_len = logs.Len()
+			lst := make([]string, log_len)
+            var index int = 0
+            logs.Do(func(elem interface{}) {
+                if elem != nil {
+                    //log.Printf("current index is %d\n", index)
+                    lst[index] = elem.(string)
+                    index += 1
+                }
+            })
+			// reset the ring
+			logs = ring.New(maxRingSize)
+            enc, err = json.Marshal(lst[0:index])
+			if err != nil {
+				log.Printf("could not convert the logs to JSON !!!")
+			}
+			out_chan <- enc
+		}
 	}
 }
 
 func HandleStats(rw http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-        log.Printf("HandleStats: got a non get request from %s\n", r.RemoteAddr)
-        return
-    }
-    fmt.Fprintln(rw, "logged up to now:", requests)
+	if r.Method != "GET" {
+		log.Printf("HandleStats: got a non get request from %s\n", r.RemoteAddr)
+		return
+	}
+	fmt.Fprintln(rw, "logged up to now:", requests)
 }
 
 func HandleLogPost(rw http.ResponseWriter, r *http.Request) {
