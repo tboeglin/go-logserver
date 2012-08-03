144f0180890116a179abc1d2a1c811ee9184ed1b
Thomas Boeglin <thomas.boeglin@gmail.com>
2012-08-03 07:46:36 +0200

    add command line options for port and backlog size

--- logserver.go
+++ logserver.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"logserver/handlers"
 	"net/http"
-    "logserver/handlers"
 )
 
 func main() {
-    // handlers
-    http.HandleFunc("/log", handlers.HandleLogPost)
-    http.HandleFunc("/stats", handlers.HandleStats)
+	var (
+		portno  int
+		logsize int
+	)
+	flag.IntVar(&portno, "port", 8888, "the port to bind to")
+	flag.IntVar(&logsize, "logsize", 1000, "the size of the backlog")
 
-    err := http.ListenAndServe(":8888", nil)
-    if err != nil {
-        log.Fatal("while listening:", err)
-    }
+	flag.Parse()
+	handlers.MaxLogSize(logsize)
+	// handlers
+	http.HandleFunc("/log", handlers.HandleLogPost)
+	http.HandleFunc("/stats", handlers.HandleStats)
+
+    log.Printf("Starting on port %d with backlog size %d\n", portno, logsize)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", portno), nil)
+	if err != nil {
+		log.Fatal("while listening:", err)
+	}
 }
